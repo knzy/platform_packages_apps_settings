f1d783d7db18dc4b8489c31cec159d61d5b1587e
knzy <lucifer@kindergarten>
2012-09-13 00:56:24 +0100

    fix the NPE introduced by volume cursor control

--- src/com/android/settings/inputmethod/InputMethodAndLanguageSettings.java
+++ src/com/android/settings/inputmethod/InputMethodAndLanguageSettings.java
@@ -185,10 +185,12 @@ public class InputMethodAndLanguageSettings extends SettingsPreferenceFragment
         }
 
         mVolumeKeyCursorControl = (ListPreference) findPreference(VOLUME_KEY_CURSOR_CONTROL);
-        mVolumeKeyCursorControl.setOnPreferenceChangeListener(this);
-        mVolumeKeyCursorControl.setValue(Integer.toString(Settings.System.getInt(getActivity()
+	if (mVolumeKeyCursorControl != null) {
+        	mVolumeKeyCursorControl.setOnPreferenceChangeListener(this);
+		mVolumeKeyCursorControl.setValue(Integer.toString(Settings.System.getInt(getActivity()
                 .getContentResolver(), Settings.System.VOLUME_KEY_CURSOR_CONTROL, 0)));
-        mVolumeKeyCursorControl.setSummary(mVolumeKeyCursorControl.getEntry());
+		mVolumeKeyCursorControl.setSummary(mVolumeKeyCursorControl.getEntry());
+	}
 
         mHandler = new Handler();
         mSettingsObserver = new SettingsObserver(mHandler, getActivity());
